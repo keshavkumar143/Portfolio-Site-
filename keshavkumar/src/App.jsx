e9ce0e9bc45c8b2e1c459e186bdbf5f0fe9826ff
KeshavKumarCG <keshav.kumar@cginfinity.com>
2025-05-16 16:11:02 +0530

    Refactor import statements and improve JSX formatting for consistency

--- keshavkumar/src/App.jsx
+++ keshavkumar/src/App.jsx
@@ -1,28 +1,26 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './Components/Navbar/Nav';
-import Home from './Components/Home/Home'
-import Contact from './Components/Contact/contact'
-import Projects from './Components/Projects/Project'
+import Home from './Components/Home/Home';
+import Contact from './Components/Contact/contact';
+import Projects from './Components/Projects/Project';
 import Skill from './Components/Skills/Skill';
 import Experience from './Components/Experience/Experience';
-import Cursor from './Components/Cursor'
+import Cursor from './Components/Cursor';
 
 function App() {
   return (
-    <>
-      <Router>
-      <Cursor/>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/skills" element={<Skill />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/experience" element={<Experience />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Cursor />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Skill />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
